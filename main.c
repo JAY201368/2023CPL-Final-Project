c2be8c589826525bc2afddce8f6cb643e0c7c6f8
JAY <1991811565@qq.com>
2024-02-03 08:32:32 +0800

    完善了一些代码, 加入一些新的设计

--- main.c
+++ main.c
@@ -13,14 +13,16 @@
 #define DEFAULT_MAXIMUM_JUMP_DISTANCE 100
 #define DEFAULT_PLATFORM_SIZE 100
 #define DEFAULT_GAIN_MOMENTUM_SPEED 100
+#define DEFAULT_PLAYER_POS_X 100
+#define DEFAULT_PLAYER_POS_Y 100
 
 // 函数宏
 // 处理SDL错误用宏封装简化
-#define CHECK_ERROR(name)                              \
-    if (!(name)) {                                     \
-        printf("Failed when creating %s!\n", (#name)); \
-        SDL_GetError();                                \
-        return 1;                                      \
+#define CHECK_ERROR(name)                               \
+    if (!(name)) {                                      \
+        SDL_Log("Failed when creating %s!\n", (#name)); \
+        SDL_GetError();                                 \
+        return 1;                                       \
     }
 
 // 枚举
@@ -55,6 +57,7 @@ typedef struct game {
     int mode;
     int score;
     int history_high;
+    bool end_game;
 } GAME;
 
 
@@ -62,8 +65,12 @@ typedef struct game {
 SDL_Window *win;
 SDL_Event MainEvent;
 SDL_Surface *bg_img = NULL;
-SDL_Texture *bg_texture;
-SDL_Renderer *Default_rdr;
+SDL_Surface *start_menu_img = NULL;
+SDL_Surface *pause_menu_img = NULL;
+SDL_Texture *bg_texture = NULL;
+SDL_Texture *start_menu_texture = NULL;
+SDL_Texture *pause_menu_texture = NULL;
+SDL_Renderer *Default_rdr = NULL;
 // TODO: 创建若干个rect类型对象, 用于指示贴图位置
 
 // 游戏状态, 玩家状态指示器
@@ -93,6 +100,10 @@ int initSDL() {
     return 0;
 }
 
+void InitPic() {
+    // TODO: 将加载单个函数的代码封装
+}
+
 int initPics() {
     // 为所有图片资源分配surface和texture
     // ③加载所有图片文件到相应画布
@@ -103,12 +114,42 @@ int initPics() {
     bg_texture = SDL_CreateTextureFromSurface(Default_rdr, bg_img);
     CHECK_ERROR(bg_texture);
 
+
+    pause_menu_img = NULL;
+    bg_texture = NULL;
+    start_menu_texture = NULL;
+    pause_menu_texture = NULL;
+
+    // 开始菜单贴图
+    // start_menu_img = IMG_Load("start_menu_img.jpg");
+    // CHECK_ERROR(start_menu_img);
+    // start_menu_texture = SDL_CreateTextureFromSurface(Default_rdr, start_menu_img);
+    // CHECK_ERROR(start_menu_img);
+
+    // 暂停菜单贴图
+    // pause_menu_img = IMG_Load("pause_menu_img.jpg");
+    // CHECK_ERROR(pause_menu_img);
+    // pause_menu_texture = SDL_CreateTextureFromSurface(Default_rdr, pause_menu_img);
+    // CHECK_ERROR(pause_menu_texture);
+
+    // 计分板贴图
+
+    // 平台贴图
+
+    // 玩家贴图
+
+    // bonus(彩蛋)贴图
+
+    //蓄力条贴图
+
     return 0;
 }
 
 void DestroyAll() {
     // 关闭所有子系统
     SDL_DestroyTexture(bg_texture);
+    SDL_DestroyTexture(start_menu_texture);
+    SDL_DestroyTexture(pause_menu_texture);
     SDL_DestroyWindowSurface(win);
     SDL_DestroyRenderer(Default_rdr);
     SDL_DestroyWindow(win);
@@ -138,7 +179,7 @@ int main() {
     // ①初始化SDL子系统
     initSDL();
 
-    // 加载背景图片
+    // 加载所有图片, 显示背景图片
     initPics();
     SDL_RenderCopy(Default_rdr, bg_texture, NULL, NULL);
     SDL_RenderPresent(Default_rdr);
@@ -149,24 +190,27 @@ int main() {
             .CurJumpDistance = 0,
             .GainMomentumSpeed = DEFAULT_GAIN_MOMENTUM_SPEED,
             .motion = common,
-            // .cur_x = ,
-            // .cur_y =
+            .cur_x = DEFAULT_PLAYER_POS_X,
+            .cur_y = DEFAULT_PLAYER_POS_Y
     };
     GAME game = {
             .history_high = 0,
             .score = 0,
-            .mode = start_menu
+            .mode = start_menu,
+            .end_game = false
     };
+
     // 游戏主体: 两个阶段(处理事件->渲染下一帧)
-    while (true) {
+    while (game.end_game == false) {
         while (SDL_PollEvent(&MainEvent)) {
             // 选择事件类型
             // 综合几个因素, 决定下一帧画面的显示内容, 用函数来实现对帧内容的编辑
             switch (MainEvent.type) {
                 case SDL_QUIT:
                     // 处理退出事件
-                    printf("QUIT GAME!\n");
-                    return 0;
+                    SDL_Log("QUIT GAME!\n");
+                    game.end_game = true;
+                    break;
                 case SDL_MOUSEMOTION: // 鼠标事件, 供测试用
                     SDL_Log("Current Mouse Position: x = %d, y = %d", MainEvent.motion.x, MainEvent.motion.y);
                     break;
@@ -189,6 +233,7 @@ int main() {
                     }
                     break;
                 case SDL_KEYUP:
+                    // 放开空格键跳跃的部分, 是否可以作为Jump函数的一部分, 而不是放在轮询过程中?
                     if (MainEvent.key.keysym.sym == SDLK_SPACE) {
                         // 放开空格 -> 跳跃
                         // TODO: 处理跳跃
